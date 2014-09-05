e42ea6d46e9231113583dac9826753a7d3adf688
hanchao <hanchao0123@hotmail.com>
2014-09-05 16:09:55 +0800

    first test node-mbtiles

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log("test");
+
+require('sqlite3').verbose();
+
+var MBTiles = require('mbtiles');
+
+var bmtilesFile = "/Users/chaohan/Documents/maptest_30c930.mbtiles";
+
+var btTiles = new MBTiles(bmtilesFile, function(err, mbtiles) {
+                if (err) throw err;
+				console.log(mbtiles);
+				
+				btTiles.getTile(0, 0, 10, function(err, tile, headers) {
+				 			                if (err) throw err;
+											console.log(tile);
+											console.log(headers);
+				 			            });
+            });
+
+console.log("test end");
