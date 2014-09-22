a1e3909243b8cb85b30f42f19433c1e5a61333f9
hanchao <hanchao0123@hotmail.com>
2014-09-22 11:48:06 +0800

    serve mbtiles

--- main.js
+++ main.js
@@ -1,20 +1,45 @@
-console.log("test");
 
 require('sqlite3').verbose();
+var express = require('express');
+var mbtiles = require('mbtiles');
 
-var MBTiles = require('mbtiles');
+var app = express();
 
 var bmtilesFile = "/Users/chaohan/Documents/maptest_30c930.mbtiles";
+var curmbtiles = new mbtiles(bmtilesFile, function(err, mbtiles) {
+	if (err) throw err;
+		console.log("open file %s succeed", bmtilesFile);
+		
+		var server = app.listen(3000, function() {
+		    console.log('Listening on port %d', server.address().port);
+		});
+});
 
-var btTiles = new MBTiles(bmtilesFile, function(err, mbtiles) {
-                if (err) throw err;
-				console.log(mbtiles);
-				
-				btTiles.getTile(0, 0, 0, function(err, tile, headers) {
-				 			                if (err) throw err;
-											console.log(tile);
-											console.log(headers);
-				 			            });
-            });
+app.get('/', function(req, res){
+  res.send('index');
+});
 
-console.log("test end");
+app.get('/hello.txt', function(req, res){
+	res.send('Hello World');
+});
+
+app.get('/:z(\\d+)/:x(\\d+)/:y(\\d+)', function(req, res){
+    var z = req.params.z | 0,
+        x = req.params.x | 0,
+        y = req.params.y | 0;
+		
+	console.log("get tile, z = %d, x = %d, y = %d", z, x, y);
+	
+	curmbtiles.getTile(z, x, y, function(err, tile, headers) {
+		if (err) {
+			res.status(404)
+				res.send(err.message);
+		}
+		else {
+			res.set(headers);
+			res.send(tile);										
+		}
+
+	 });
+});
+
